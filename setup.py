7d07e491cc95fc4b9c7402006e0bcec4958b4d46
Necdet Can Ateşman <can@strg.at>
2017-05-23 14:51:45 +0200

    Release: 0.1.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.mustache',
-    version='0.1.0',
+    version='0.1.1',
     description='Renderer for mustache files for The SCORE Framework',
     long_description=README,
     author='strg.at',
